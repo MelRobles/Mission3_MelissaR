5e4c54e8af37e4ec94fd894ea826fea8a46a8353
MelRobles <testMelEmail@test.com>
2022-01-19 23:56:55 -0700

    Add project files.

---
+++ MyFirstASPNetWebApp/Controllers/BlahController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyFirstASPNetWebApp.Models;
+using System.Threading.Tasks;
+
+namespace MyFirstASPNetWebApp.controllers
+{
+    public class blahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculator(CalculatorModel calcNum)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.corrent = ("Your Final Grade Is: ");
+            }
+            return View(calcNum);
+        }
+    }
+}
---
+++ MyFirstASPNetWebApp/Models/CalculatorModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+
+
+namespace MyFirstASPNetWebApp.Models
+//namespace RangeValidationAPI.Models
+{
+    public class CalculatorModel
+    {
+        
+        [Required(ErrorMessage = "Please enter your number")]
+        [Range(0, 100, ErrorMessage = "*Enter a number between 0 and 100*")]
+        public int assign { get; set; }
+        public int group { get; set; }
+        public int quiz { get; set; }
+        public int exam { get; set; }
+        public int intex { get; set; }
+    }
+}
